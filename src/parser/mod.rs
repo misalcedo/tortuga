bf0f8ef36b15b50aa7a5f4220df65d74cb3180ac
Miguel D. Salcedo <miguel@salcedo.cc>
2020-04-27 21:13:15 -0400

    Add skeleton for nom based parser.

---
+++ src/parser/mod.rs
@@ -0,0 +1,7 @@
+mod ast;
+
+/// Parses an input stream using the language's grammar into an abstract syntax tree.
+pub fn parse() -> ast::AbstractSyntaxTree {
+    println!("Hi!");
+    ast::AbstractSyntaxTree {}
+}
