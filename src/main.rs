f3292614d9ef0e798cf0bdb747b5e4555d004c61
Miguel D. Salcedo <miguel@salcedo.cc>
2020-04-28 21:00:09 -0400

    Interpreter that does not compile.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,48 @@
+use std::io::{self, Read};
+
 mod parser;
 
 fn main() {
-    parser::parse();
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer).expect("Unable to read from stdin.");
+
+    let actions = parser::parse(buffer.as_str()).expect("Unable to parse the file.");
+
+    for (reference, message) in actions.iter() {
+        match reference {
+            &"add" => {
+                let total: u64 = message.iter().sum();
+                println!("{}", total);
+            },
+            &"subtract" => {
+                let iterator = message.iter();
+
+                match iterator.next() {
+                    Some(value) => {
+                        let total: u64 = iterator.sum();
+                        println!("{}", value - total)
+                    },
+                    None => eprintln!("Must pass at least one value to subtract.")
+                };
+            },
+            &"divide" => {
+                let iterator = message.iter();
+
+                match iterator.next() {
+                    Some(value) => {
+                        let total: u64 = iterator.product();
+                        println!("{}", value / total)
+                    },
+                    None => eprintln!("Must pass at least one value to subtract.")
+                };
+            },
+            &"multiply" => {
+                let total: u64 = message.iter().product();
+                println!("{}", total);
+            }
+        };
+    }
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,22 +1,31 @@
-use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::character::complete::{alpha1, digit1, space0, space1};
+use nom::character::complete::{alpha1, digit1, line_ending, multispace0, space0, space1};
 use nom::combinator::{map_res, verify};
 use nom::multi::separated_list0;
+use nom::sequence::{delimited, separated_pair, terminated};
 use nom::IResult;
 
-mod ast;
-
 /// Parses an input stream using the language's grammar into an abstract syntax tree.
-pub fn parse() -> ast::AbstractSyntaxTree {
-    ast::AbstractSyntaxTree {}
+pub fn parse(input: & str) -> Result<Vec<(&str, Vec<u64>)>, &'static str> {
+    match terminated(parse_behavior, multispace0)(input) {
+        Ok((remaining, result)) => Ok(result),
+        Err(e) => {
+            eprintln!("{}", e);
+            Err("Error occurred.")
+        }
+    }
 }
 
-fn parse_behavior() {
+fn parse_behavior(input: &str) -> IResult<&str, Vec<(&str, Vec<u64>)>> {
+    separated_list0(line_ending, parse_send_message)(input)
 }
 
 fn parse_send_message(input: &str) -> IResult<&str, (&str, Vec<u64>)> {
-    Ok((input, (input, vec![])))
+    delimited(
+        delimited(space0, tag("("), space0),
+        separated_pair(parse_reference, space1, parse_message),
+        delimited(space0, tag(")"), space0)
+    )(input)
 }
 
 fn parse_reference(input: &str) -> IResult<&str, &str> {
@@ -65,5 +74,25 @@ mod tests {
         let expected = vec![1, 4, 0];
 
         assert_eq!(parse_message(text), Ok((" )", expected)));
+    }
+
+    #[test]
+    fn parse_valid_send_message() {
+        let text = "( add 1 4 0 )";
+        let expected = ("add", vec![1, 4, 0]);
+
+        assert_eq!(parse_send_message(text), Ok(("", expected)));
+    }
+
+    #[test]
+    fn parse_valid_behavior() {
+        let text = "( add 1 4 0 )\n(substract 5 3 )   \r\n   ( multiply 2 2)  \r\n ";
+        let expected = vec![
+            ("add", vec![1, 4, 0]),
+            ("substract", vec![5, 3]),
+            ("multiply", vec![2, 2]),
+        ];
+
+        assert_eq!(parse_behavior(text), Ok(("", expected)));
     }
 }
