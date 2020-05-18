f56bed425c8e488836815a536cd46a6600f6bb98
Miguel D. Salcedo <miguel@salcedo.cc>
2020-05-18 15:49:11 -0400

    Add options to cli. add wat compiler to library. start 2nd blog post.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,19 +1,37 @@
+use core::mem::{forget, size_of};
+use std::error::Error;
 use std::fs::read;
 use std::path::Path;
-use std::error::Error;
-use core::mem::{forget, size_of};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "tortuga",
+    about = "An actor-based system built on top of WASM technologies."
+)]
+struct Options {
+    #[structopt(short, long, parse(from_os_str))]
+    text_actors: Vec<PathBuf>,
+    #[structopt(short, long, parse(from_os_str))]
+    binary_actors: Vec<PathBuf>,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let opt = Options::from_args();
+
+    println!("{:?}", opt);
+
     let path = Path::new("./examples/target/wasm32-unknown-unknown/debug/");
 
     tortuga::run(&read(path.join("echo.wasm"))?, "Hello, World!".as_bytes())?;
-    
+
     let mut numbers = [42, 7, 1, 5];
     let ptr = numbers.as_mut_ptr() as *mut u8;
     let length = numbers.len() * size_of::<u32>();
-    
+
     forget(numbers);
-    
+
     let bytes = unsafe { Vec::from_raw_parts(ptr, length, length) };
 
     tortuga::run(&read(path.join("add.wasm"))?, &bytes)?;
