472ca2a9f595fc458a395826dc67327a2cf7c1e1
Miguel D. Salcedo <miguel@salcedo.cc>
2020-05-19 19:38:32 -0400

    Make examples into executables with no_std.

--- examples/echo/src/main.rs
+++ examples/echo/src/main.rs
@@ -1,3 +1,8 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
 #[link(wasm_import_module = "system")]
 extern "C" {
     fn send(address: *const u8, length: usize);
@@ -6,4 +11,9 @@ extern "C" {
 #[no_mangle]
 pub unsafe fn receive(address: *const u8, length: usize) {
     send(address, length);
+}
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
 }
