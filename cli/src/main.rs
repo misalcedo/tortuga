6e8498e0fe7971af4e2305d96170f05e24f3c884
Miguel D. Salcedo <miguel@salcedo.cc>
2020-05-19 20:33:58 -0400

    remove extra code from CLI.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,5 @@
-use core::mem::{forget, size_of};
 use std::error::Error;
 use std::fs::read;
-use std::path::Path;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -11,41 +9,26 @@ use structopt::StructOpt;
     about = "An actor-based system built on top of WASM technologies."
 )]
 struct Options {
-    #[structopt(short, long, parse(from_os_str))]
-    text_actors: Vec<PathBuf>,
-    #[structopt(short, long, parse(from_os_str))]
-    binary_actors: Vec<PathBuf>,
+    #[structopt(
+        short,
+        long,
+        default_value = "./resources/wasm/echo.wat",
+        parse(from_os_str)
+    )]
+    actor: PathBuf,
+    #[structopt(short, long, default_value = "Hello, World!")]
+    message: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let opt = Options::from_args();
-
-    println!("{:?}", opt);
-
-    let path = Path::new("./examples/target/wasm32-unknown-unknown/debug/");
-    let resource_path = Path::new("./resources/wasm/");
-    let echo_module = &read(resource_path.join("echo.wat"))?;
-    let add_module = &read(path.join("add.wasm"))?;
+    let options = Options::from_args();
+    let echo_module = &read(options.actor)?;
 
     let mut system = tortuga::System::new();
+    let actor = system.register(echo_module)?;
 
-    let echo = system.register(echo_module)?;
-    let add = system.register(add_module)?;
-
-    system.send(echo, "Hello, World!".as_bytes())?;
-
-    let mut numbers = [42, 7, 1, 5];
-    let ptr = numbers.as_mut_ptr() as *mut u8;
-    let length = numbers.len() * size_of::<u32>();
-
-    forget(numbers);
-
-    let bytes = unsafe { Vec::from_raw_parts(ptr, length, length) };
-
-    system.send(add, &bytes)?;
-
-    system.run(echo)?;
-    system.run(add)?;
+    system.send(actor, options.message.as_bytes())?;
+    system.run(actor)?;
 
     Ok(())
 }
