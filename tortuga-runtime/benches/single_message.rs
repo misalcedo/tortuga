68287379f6eb4863d12221cef816b7c6a993154d
Miguel D. Salcedo <miguel@salcedo.cc>
2020-11-29 11:28:32 -0500

    Fix import path for benchmark

--- tortuga-runtime/benches/single_message.rs
+++ tortuga-runtime/benches/single_message.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use tortuga::System;
+use tortuga_runtime::System;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut system = System::new(1);
