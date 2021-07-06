c748b43a72398cf87acaadd4a024cf8f534d2e47
Miguel D. Salcedo <misalcedo@github.com>
2021-07-06 16:26:11 -0400

    Delete guest crate.

--- tortuga-guest/examples/echo.rs
+++
@@ -1,9 +0,0 @@
-#[link(wasm_import_module = "system")]
-extern "C" {
-    fn send(address: *const u8, length: usize);
-}
-
-#[no_mangle]
-pub unsafe fn receive(address: *const u8, length: usize) {
-    send(address, length);
-}
