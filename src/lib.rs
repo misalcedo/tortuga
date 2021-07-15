0f8501120ecdaa172e37723d4ce3381bd621e9fe
Miguel D. Salcedo <miguel@salcedo.cc>
2021-07-15 15:35:02 -0400

    Add a build CLI command. Implement fake compilation to reduce number of unused fields.

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,18 @@ mod compiler;
 mod queue;
 mod web_assembly;
 
+use compiler::Compiler;
 pub use queue::Envelope;
+use std::fs::{create_dir_all, File};
+use std::path::Path;
+
+pub fn compile(input: &Path, output: &Path) {
+    create_dir_all(output).unwrap();
+
+    let compiler = Compiler::new();
+    let filename = output.to_path_buf().join("example.wasm");
+    let mut file = File::create(filename.as_path()).unwrap();
+    let input: Vec<u8> = Vec::new();
+
+    compiler.compile(&input.as_slice(), &mut file).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,47 @@
-fn main() {}
+use clap::{App, Arg, SubCommand};
+use std::path::Path;
+use tortuga::compile;
+
+fn main() {
+    let matches = App::new("Tortuga")
+        .version("0.2.0")
+        .author("Miguel D. Salcedo <miguel@salcedo.cc>")
+        .about("Compiler and runtime executable for the Tortuga programming language.")
+        .subcommand(
+            SubCommand::with_name("build")
+                .about("Compiles the input directory.")
+                .arg(
+                    Arg::with_name("input")
+                        .short("i")
+                        .index(1)
+                        .value_name("PATH")
+                        .default_value("./src/")
+                        .help("Sets a custom input directory for compilation.")
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("output")
+                        .short("o")
+                        .index(2)
+                        .value_name("PATH")
+                        .default_value("./out/")
+                        .help("Sets a custom output directory for compilation.")
+                        .takes_value(true),
+                ),
+        )
+        .get_matches();
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    if let Some(matches) = matches.subcommand_matches("build") {
+        let output = matches.value_of("output").map(Path::new).unwrap();
+        let input = matches.value_of("input").map(Path::new).unwrap();
+
+        compile(input, output);
+    } else {
+        println!(
+            "Invalid subcommand name: {}",
+            matches.subcommand_name().unwrap_or("")
+        );
+    }
+}
