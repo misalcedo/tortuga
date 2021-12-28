fdbaf900675421d7dcf80a53c1b6fbadf43ccd98
Miguel D. Salcedo <misalcedo@github.com>
2021-12-27 20:54:06 -0500

    Document the regular expression for numbers.

--- src/compiler/number.rs
+++ src/compiler/number.rs
@@ -1,7 +1,7 @@
 use crate::compiler::errors::lexical::ErrorKind;
 use crate::runtime::Number;
 use lazy_static::lazy_static;
-use regex::{Match, Regex};
+use regex::{Captures, Match, Regex};
 use std::str::FromStr;
 
 /// Base 10 (i.e. decimal). The default base for all numbers.
@@ -10,28 +10,31 @@ pub const DECIMAL: u32 = 10;
 /// The largest supported radix for numbers with an explicit base.
 pub const MAX_RADIX: u32 = 36;
 
+const DEFAULT_NUMBER_PART: &str = "0";
+const DEFAULT_RADIX: &str = "10";
+
 impl FromStr for Number {
     type Err = ErrorKind;
 
     fn from_str(number: &str) -> Result<Self, Self::Err> {
         lazy_static! {
-            static ref NUMBER_REGEX: Regex = Regex::new(r###"^(?:([Ii]|[[:digit:]--0][[:digit:]]*)#)?(0?|(?:[[:alnum:]--0][[:alnum:]]*))(?:\.([[:alnum:]]*))?$"###)
-                .expect("Invalid regular expression for NUMBER token.");
+            static ref NUMBER_REGEX: Regex = Regex::new(
+                r###"(?x)
+                    ^
+                    (?: ( [[:digit:]--0] [[:digit:]]{0, 1}) \# )? # Optional non-zero radix; at most 2 digits.
+                    ( 0? | (?: [[:alnum:]--0] [[:alnum:]]* ) ) # Interger part; optional when the fractional part is present.
+                    (?: \. ( [[:alnum:]]* ) )? # Fractional part; optional when the integer part is present.
+                    $
+                "###
+            )
+            .expect("Invalid regular expression for NUMBER token.");
         }
 
         let captures = NUMBER_REGEX.captures(number).ok_or(ErrorKind::Number)?;
 
-        let radix_part = captures.get(1).as_ref().map(Match::as_str).unwrap_or("10");
-        let integer_part = captures
-            .get(2)
-            .as_ref()
-            .map(Match::as_str)
-            .unwrap_or_default();
-        let fraction_part = captures
-            .get(3)
-            .as_ref()
-            .map(Match::as_str)
-            .unwrap_or_default();
+        let radix_part = get_match(&captures, 1).unwrap_or(DEFAULT_RADIX);
+        let integer_part = get_match(&captures, 2);
+        let fraction_part = get_match(&captures, 3);
 
         let radix: u32 = radix_part.parse().map_err(|_| ErrorKind::Number)?;
 
@@ -39,21 +42,23 @@ impl FromStr for Number {
             return Err(ErrorKind::Number);
         }
 
-        let integer = if integer_part.is_empty() {
-            0
-        } else {
-            i128::from_str_radix(integer_part, radix).map_err(|_| ErrorKind::Number)?
-        };
-        let fraction = if fraction_part.is_empty() {
-            0
-        } else {
-            i128::from_str_radix(fraction_part, radix).map_err(|_| ErrorKind::Number)?
-        };
+        let integer = i128::from_str_radix(integer_part.unwrap_or(DEFAULT_NUMBER_PART), radix)
+            .map_err(|_| ErrorKind::Number)?;
+        let fraction = i128::from_str_radix(fraction_part.unwrap_or(DEFAULT_NUMBER_PART), radix)
+            .map_err(|_| ErrorKind::Number)?;
 
-        if integer_part.is_empty() && fraction_part.is_empty() {
+        if integer_part.is_none() && fraction_part.is_none() {
             Err(ErrorKind::Number)
         } else {
             Ok(42.into())
         }
     }
+}
+
+fn get_match<'a>(captures: &Captures<'a>, index: usize) -> Option<&'a str> {
+    captures
+        .get(index)
+        .as_ref()
+        .map(Match::as_str)
+        .filter(|s| !s.is_empty())
 }
