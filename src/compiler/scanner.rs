0779168ebd95e26ae2e667df50b557d563c0252d
Miguel D. Salcedo <misalcedo@github.com>
2021-12-27 20:35:33 -0500

    Remove imaginary numbers. Need to reduce scope.

--- src/compiler/scanner.rs
+++ src/compiler/scanner.rs
@@ -20,17 +20,8 @@ impl FromStr for Number {
         }
 
         let captures = NUMBER_REGEX.captures(number).ok_or(ErrorKind::Number)?;
-        println!("NUMBER: {:?}", captures);
 
-        let mut radix_part = captures.get(1).as_ref().map(Match::as_str).unwrap_or("10");
-
-        let imaginary = radix_part == "i" || radix_part == "I";
-
-        if imaginary {
-            radix_part = "10";
-        }
-
-        let radix: u32 = radix_part.parse().map_err(|_| ErrorKind::Number)?;
+        let radix_part = captures.get(1).as_ref().map(Match::as_str).unwrap_or("10");
         let integer_part = captures
             .get(2)
             .as_ref()
@@ -41,6 +32,8 @@ impl FromStr for Number {
             .as_ref()
             .map(Match::as_str)
             .unwrap_or_default();
+
+        let radix: u32 = radix_part.parse().map_err(|_| ErrorKind::Number)?;
 
         if radix > MAX_RADIX {
             return Err(ErrorKind::Number);
@@ -112,23 +105,12 @@ impl<'a> Iterator for Scanner<'a> {
                 '<' => self.scan_less_than(),
                 '>' => self.scan_greater_than(),
                 '.' => self.scan_fractional_number(),
-                'i' | 'I' => self.scan_imaginary_or_identifier(),
                 d if d.is_ascii_digit() => self.scan_number(),
                 s if s.is_xid_start() => self.scan_identifier(),
                 _ => self.scan_invalid(),
             };
 
             return Some(result);
-        }
-    }
-}
-
-impl<'a> Scanner<'a> {
-    fn scan_imaginary_or_identifier(&mut self) -> LexicalResult {
-        if self.input.peek() == Some('#') {
-            self.scan_number()
-        } else {
-            self.scan_identifier()
         }
     }
 }
@@ -362,18 +344,6 @@ mod tests {
         validate_number("30#0.5");
         validate_number("36#10000.5002");
         validate_number("32#7.002");
-
-        validate_number("i#0");
-        validate_number("i#2");
-        validate_number("i#21");
-        validate_number("i#100");
-        validate_number("i#.100");
-        validate_number("I#.5");
-        validate_number("i#1.0");
-        validate_number("i#4.5");
-        validate_number("i#0.5");
-        validate_number("i#10000.5002");
-        validate_number("I#7.002");
     }
 
     fn invalidate_number(number: &str) {
@@ -397,8 +367,6 @@ mod tests {
         invalidate_number("0008");
         invalidate_number("37#1.0");
         invalidate_number("2#4.0");
-        invalidate_number("i#");
-        invalidate_number("I#.");
     }
 
     #[test]
