213aeab12cea6a3b3e134fd30b7748c4c4ac8728
Miguel D. Salcedo <misalcedo@github.com>
2021-12-29 16:35:28 -0500

    Document comparisons

--- src/grammar/syntax/comparison.rs
+++ src/grammar/syntax/comparison.rs
@@ -1,13 +1,36 @@
-use crate::grammar::Expression;
+use crate::grammar::syntax::expression::{Expression, List};
 
 /// A pair of a comparison operator and the right-hand side expression to compare against.
-pub type Comparison = (Operator, Expression);
+pub struct Comparison(Operator, Expression);
+
+impl Comparison {
+    /// Creates a new operator and right-hand side pair for the comparison rule.
+    pub fn new(operator: Operator, rhs: Expression) -> Self {
+        Comparison(operator, rhs)
+    }
+}
 
 /// program → expression ( comparison expression )+ EOF ;
 pub struct Comparisons {
     lhs: Expression,
-    first: Comparison,
-    rest: Comparison,
+    comparison: List<Comparison>,
+}
+
+impl Comparisons {
+    /// Create a new `Comparisons` instance.
+    pub fn new(lhs: Expression, comparison: List<Comparison>) -> Self {
+        Comparisons { lhs, comparison }
+    }
+
+    /// The left-hand side of this sequenece of `Comparisons`.
+    pub fn lhs(&self) -> &Expression {
+        &self.lhs
+    }
+
+    /// The list of comparison operations to perform.
+    pub fn comparisons(&self) -> &List<Comparison> {
+        &self.comparison
+    }
 }
 
 /// Comparison operators.
