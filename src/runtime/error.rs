6477b6bd2acb25a8385bc0002c6d12ea2e8fa5df
Miguel D. Salcedo <misalcedo@github.com>
2022-01-02 14:17:58 -0500

    remove unknown runtime error

--- src/runtime/error.rs
+++ src/runtime/error.rs
@@ -20,13 +20,11 @@ pub enum RuntimeError {
     UnexpectedType(Value, String),
     #[error("Called function {0} with the wrong number of argument(s) (expected {1}, but called with {2}.")]
     MismatchedArity(String, usize, usize),
-    #[error("No definition found for function {0} with the given arguments: {}.", stringify_arguments(.1))]
+    #[error("No definition found for function {0} with the given arguments: {}.", stringify_arguments(.1.as_slice()))]
     NoMatchingDefinition(String, Vec<Value>),
-    #[error("Encountered an unknown runtime error.")]
-    Unknown,
 }
 
-fn stringify_arguments(arguments: &Vec<Value>) -> String {
+fn stringify_arguments(arguments: &[Value]) -> String {
     arguments
         .iter()
         .map(Value::to_string)
