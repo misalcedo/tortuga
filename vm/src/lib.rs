ab49119f059b77cca11b85abb24a3d6134b3e503
Miguel D. Salcedo <misalcedo@github.com>
2022-07-25 08:16:31 -0400

    Add unversioned files.

---
+++ vm/src/lib.rs
@@ -0,0 +1,24 @@
+mod closure;
+mod courier;
+mod error;
+mod frame;
+mod identifier;
+mod machine;
+mod operation;
+mod program;
+mod value;
+
+pub use courier::Courier;
+pub(crate) use frame::CallFrame;
+pub use identifier::Identifier;
+pub use program::Program;
+pub use value::Value;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
---
+++ vm/src/machine.rs
@@ -0,0 +1,105 @@
+pub use crate::error::{ErrorKind, RuntimeError};
+pub use crate::CallFrame;
+pub use crate::Courier;
+pub use crate::Identifier;
+pub use crate::Program;
+pub use crate::Value;
+
+#[derive(Clone, Debug, Default)]
+pub struct VirtualMachine<Courier> {
+    courier: Courier,
+    mailbox: Vec<Value>,
+    code: Code,
+    stack: Vec<Value>,
+    frames: Vec<CallFrame>,
+}
+
+type RuntimeResult<T> = Result<T, RuntimeError>;
+type OperationResult = RuntimeResult<()>;
+
+impl<C: Courier> VirtualMachine<C> {
+    const OPERATIONS_TABLE: [fn(&mut VirtualMachine<C>) -> OperationResult; 2] =
+        [VirtualMachine::pop, VirtualMachine::send];
+
+    pub fn process(&mut self) -> Result<Value, RuntimeError> {
+        self.frames.push(CallFrame {});
+        self.stack.push(Value::Number);
+        self.stack.push(Value::Identifier);
+        self.stack.push(Value::Closure);
+
+        let operation = self.get_operation(0)?;
+
+        operation(self)?;
+
+        let operation = self.get_operation(1)?;
+
+        operation(self)?;
+
+        if self.stack.is_empty() {
+            Ok(Value::Closure)
+        } else {
+            Err(ErrorKind::Crash.into())
+        }
+    }
+
+    /// Get the operation for the given operation code.
+    fn get_operation(
+        &self,
+        index: usize,
+    ) -> RuntimeResult<&fn(&mut VirtualMachine<C>) -> OperationResult> {
+        Self::OPERATIONS_TABLE
+            .get(index)
+            .ok_or_else(|| RuntimeError::from(ErrorKind::UnsupportedOperation(index)))
+    }
+
+    pub fn deliver(&mut self, value: Value) {
+        self.mailbox.push(value);
+    }
+
+    /// Pops the top of the stack and drops the value.
+    pub fn pop(&mut self) -> OperationResult {
+        self.stack.pop();
+
+        Ok(())
+    }
+
+    /// Sends a message to the identifier at the top of the value stack.
+    pub fn send(&mut self) -> OperationResult {
+        let identifier = self.pop_identifier()?;
+        let message = self.pop_value()?;
+
+        self.courier.deliver(identifier, message);
+
+        Ok(())
+    }
+
+    /// Pops a generic value from the stack.
+    fn pop_value(&mut self) -> RuntimeResult<Value> {
+        self.stack
+            .pop()
+            .ok_or_else(|| RuntimeError::from(ErrorKind::EmptyStack))
+    }
+
+    /// Pops an identifier from the value stack.
+    fn pop_identifier(&mut self) -> RuntimeResult<Identifier> {
+        self.pop_value()?
+            .try_into()
+            .map_err(|value| RuntimeError::from(ErrorKind::ExpectedIdentifier(value)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let mut machine: VirtualMachine<()> = VirtualMachine::default();
+
+        machine.deliver(Value::default());
+
+        let result = machine.process();
+
+        assert_eq!(result, Ok(Value::Closure))
+    }
+}
