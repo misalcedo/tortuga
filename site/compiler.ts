71bb45aee9e3ed7d5f0a310e7c94fafde87c0d4d
Miguel D. Salcedo <misalcedo@github.com>
2022-08-21 11:58:07 -0400

    Compile wasm and update page.

--- site/compiler.ts
+++ site/compiler.ts
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+import init from "./pkg/tortuga_site.js";
+import { run }  from "./pkg/tortuga_site.js";
+
+export function compileThenRun() {
+    init("./pkg/tortuga_site_bg.wasm")
+        .then(compileThenRunWasm)
+        .catch(console.error);
+
+    return false;
+}
+
+function compileThenRunWasm() {
+    const input = document.getElementById("code") as HTMLTextAreaElement;
+    const code = input.value;
+    const result = run(code);
+    const output = document.getElementById("output") as HTMLPreElement;
+
+    output.innerText = result;
+}
