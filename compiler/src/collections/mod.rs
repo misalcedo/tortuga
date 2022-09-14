1f7771c834afe25ca8e557b39d3b9af3dc346744
Miguel D. Salcedo <misalcedo@github.com>
2022-09-14 09:14:43 -0400

    Implement a tree data structure without RC. The tree can only be iterated from a parent node (no back links).

--- compiler/src/collections/mod.rs
+++ compiler/src/collections/mod.rs
@@ -1,6 +1,7 @@
 //! Data structures used throughout the compiler
 mod indices;
 mod stack;
+mod tree;
 
 pub use indices::IndexedSet;
 pub use stack::NonEmptyStack;
---
+++ compiler/src/collections/tree.rs
@@ -0,0 +1,69 @@
+use crate::compiler::Excerpt;
+use std::fmt::{Display, Formatter};
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+pub struct Tree<Data> {
+    data: Data,
+    children: Vec<Self>,
+}
+
+impl<D> From<D> for Tree<D> {
+    fn from(data: D) -> Self {
+        Tree {
+            data,
+            children: vec![],
+        }
+    }
+}
+
+impl<D> Tree<D> {
+    pub fn insert(&mut self, data: D) -> &mut Tree<D> {
+        let index = self.children.len();
+
+        self.children.push(Tree::from(data));
+
+        &mut self.children[index]
+    }
+
+    pub fn data(&self) -> &D {
+        &self.data
+    }
+
+    pub fn height(&self) -> usize {
+        1 + self
+            .children
+            .iter()
+            .map(Tree::height)
+            .max()
+            .unwrap_or_default()
+    }
+
+    pub fn children(&self) -> &[Tree<D>] {
+        self.children.as_slice()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn math() {
+        let source = "x = 3 + 2\nx * 5";
+        let mut program = Tree::from(source);
+
+        let equality = program.insert("=");
+        equality.insert("x");
+
+        let sum = equality.insert("+");
+        sum.insert("3");
+        sum.insert("2");
+
+        let product = program.insert("*");
+        product.insert("x");
+        product.insert("5");
+
+        assert_eq!(program.height(), 4);
+    }
+}
