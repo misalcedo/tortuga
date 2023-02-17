aa9f393915a64d9fd5ce5ab3d58dcb10fa859c45
Miguel D. Salcedo <misalcedo@github.com>
2023-02-17 15:57:00 -0500

    Replace all code with a simple embeddedable runtime.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
-//! Public interface of the tortuga compiler.
+//! Public interface of the tortuga compiler and runtime.
 
-pub mod about;
-pub mod compiler;
-
-#[cfg(feature = "cli")]
-pub mod display;
-
-pub mod grammar;
 pub mod runtime;
-
-pub use about::*;
-pub use compiler::{Kind, LexicalError, ParseNumberError, Parser, Scanner, SyntacticalError};
-
-#[cfg(feature = "cli")]
-pub use display::PrettyPrinter;
-
-pub use grammar::syntax::Program;
-pub use runtime::{Interpreter, RuntimeError, Value};
--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,19 +1,67 @@
-//! Numbers, tuples, and other runtime data types necessary for compiling Tortuga programs.
+//! The embedding runtime for the Tortuga WASM modules.
 
-mod environment;
-mod epsilon;
-mod error;
-mod function;
-mod interpret;
-mod number;
-mod tolerance;
-mod value;
+use wasmtime::{Caller, Config, Engine, Linker, Module, Store};
 
-pub use environment::{Environment, FunctionReference};
-pub use epsilon::EpsilonOperator;
-pub use error::RuntimeError;
-pub use function::Function;
-pub use interpret::Interpreter;
-pub use number::Number;
-pub use tolerance::Tolerance;
-pub use value::Value;
+#[derive(Default)]
+pub struct Runtime {
+    engine: Engine,
+}
+
+pub struct Shell {
+    module: Module,
+}
+
+impl Runtime {
+    pub fn load(&mut self, code: impl AsRef<[u8]>) -> Shell {
+        // Modules can be compiled through either the text or binary format
+        let module = Module::new(&self.engine, code).unwrap();
+
+        Shell { module }
+    }
+
+    pub fn execute<Data>(&mut self, shell: &Shell, data: Data) -> i32 {
+        // All wasm objects operate within the context of a "store". Each
+        // `Store` has a type parameter to store host-specific data, which in
+        // this case we're using `4` for.
+        let mut store = Store::new(&self.engine, data);
+
+        // Create a `Linker` which will be later used to instantiate this module.
+        // Host functionality is defined by name within the `Linker`.
+        let mut linker = Linker::new(&self.engine);
+
+        linker
+            .func_wrap("host", "hello", |_: Caller<'_, Data>, x: i32| x * 2)
+            .unwrap();
+
+        let instance = linker.instantiate(&mut store, &shell.module).unwrap();
+        let hello = instance
+            .get_typed_func::<(), i32>(&mut store, "hello")
+            .unwrap();
+
+        // And finally we can call the wasm!
+        hello.call(&mut store, ()).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use wasmtime::Module;
+
+    #[test]
+    fn execute_shell() {
+        let code = r#"
+        (module
+            (import "host" "hello" (func $host_hello (param i32)))
+
+            (func (export "hello")
+                i32.const 3
+                call $host_hello)
+        )
+    "#;
+        let mut runtime = Runtime::default();
+        let shell = runtime.load(code);
+
+        assert_eq!(runtime.execute(&shell, 42), 6)
+    }
+}
