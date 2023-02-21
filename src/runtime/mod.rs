2258550bdbe20792f66d398002d3a3477452068c
Miguel D. Salcedo <misalcedo@github.com>
2023-02-21 16:02:07 -0500

    Define modules as handling a single unit of work (request-response) and testing setting the response status in a host function.

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,6 +1,7 @@
 //! The embedding runtime for the Tortuga WASM modules.
 
-use wasmtime::{Caller, Config, Engine, Linker, Module, Store};
+use std::collections::HashMap;
+use wasmtime::{Caller, Engine, Linker, Module, Store};
 
 #[derive(Default)]
 pub struct Runtime {
@@ -11,6 +12,93 @@ pub struct Shell {
     module: Module,
 }
 
+/// HTTP defines a set of request methods to indicate the desired action to be performed for a given resource.
+/// Although they can also be nouns, these request methods are sometimes referred to as HTTP verbs.
+/// Each of them implements a different semantic, but some common features are shared by a group of them:
+/// e.g. a request method can be safe, idempotent, or cacheable.
+///
+/// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+#[derive(Debug, Default, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub enum Method {
+    /// The `GET` method requests a representation of the specified resource. Requests using GET should only retrieve data.
+    #[default]
+    Get,
+    /// The `HEAD` method asks for a response identical to a `GET` request, but without the response body.
+    Head,
+    /// The `POST` method submits an entity to the specified resource, often causing a change in state or side effects on the server.
+    Post,
+    /// The `PUT` method replaces all current representations of the target resource with the request payload.
+    Put,
+    /// The `DELETE` method deletes the specified resource.
+    Delete,
+    /// The `CONNECT` method establishes a tunnel to the server identified by the target resource.
+    Connect,
+    /// The `OPTIONS` method describes the communication options for the target resource.
+    Options,
+    /// The `TRACE` method performs a message loop-back test along the path to the target resource.
+    Trace,
+    /// The `PATCH` method applies partial modifications to a resource.
+    Patch,
+    Custom(String),
+}
+
+#[derive(Debug, Default, Eq, PartialEq, Ord, PartialOrd, Copy, Clone)]
+pub enum Status {
+    #[default]
+    Ok,
+    BadRequest,
+    ServerError,
+    Custom(u16),
+}
+
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+pub struct Request {
+    method: Method,
+    message: Message,
+}
+
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+pub struct Response {
+    status: Status,
+    message: Message,
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub enum HeaderName {
+    Authorization,
+    Accept,
+    Link,
+    Custom(String),
+}
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub enum HeaderValue {
+    Single(String),
+    Multiple(Vec<String>),
+}
+
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+pub struct Headers {
+    headers: HashMap<HeaderName, HeaderValue>,
+}
+
+#[derive(Debug, Default, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct Body {
+    bytes: Vec<u8>,
+}
+
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+pub struct Message {
+    headers: Headers,
+    body: Body,
+}
+
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+pub struct UnitOfWork {
+    request: Request,
+    response: Response,
+}
+
 impl Runtime {
     pub fn load(&mut self, code: impl AsRef<[u8]>) -> Shell {
         // Modules can be compiled through either the text or binary format
@@ -19,10 +107,10 @@ impl Runtime {
         Shell { module }
     }
 
-    pub fn execute<Data>(&mut self, shell: &Shell, data: Data) -> i32 {
+    pub fn execute(&mut self, shell: &Shell, data: UnitOfWork) -> UnitOfWork {
         // All wasm objects operate within the context of a "store". Each
         // `Store` has a type parameter to store host-specific data, which in
-        // this case we're using `4` for.
+        // this case we're using the unit of work for.
         let mut store = Store::new(&self.engine, data);
 
         // Create a `Linker` which will be later used to instantiate this module.
@@ -30,23 +118,31 @@ impl Runtime {
         let mut linker = Linker::new(&self.engine);
 
         linker
-            .func_wrap("host", "hello", |_: Caller<'_, Data>, x: i32| x * 2)
+            .func_wrap(
+                "host",
+                "hello",
+                |mut caller: Caller<'_, UnitOfWork>, x: u32| {
+                    let data = caller.data_mut();
+                    data.response.status = Status::Custom((x * 2) as u16);
+                },
+            )
             .unwrap();
 
         let instance = linker.instantiate(&mut store, &shell.module).unwrap();
         let hello = instance
-            .get_typed_func::<(), i32>(&mut store, "hello")
+            .get_typed_func::<(), ()>(&mut store, "hello")
             .unwrap();
 
         // And finally we can call the wasm!
-        hello.call(&mut store, ()).unwrap()
+        hello.call(&mut store, ()).unwrap();
+
+        store.into_data()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use wasmtime::Module;
 
     #[test]
     fn execute_shell() {
@@ -61,7 +157,10 @@ mod tests {
     "#;
         let mut runtime = Runtime::default();
         let shell = runtime.load(code);
+        let mut uow = UnitOfWork::default();
 
-        assert_eq!(runtime.execute(&shell, 42), 6)
+        uow.response.status = Status::Custom(6);
+
+        assert_eq!(runtime.execute(&shell, UnitOfWork::default()), uow)
     }
 }
