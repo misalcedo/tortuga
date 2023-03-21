ac5148eca5c6b71a3368e87886af41755bd45b59
Miguel D. Salcedo <misalcedo@github.com>
2023-03-21 13:35:35 -0400

    Remove read and write from messages.

--- examples/echo/src/main.rs
+++ examples/echo/src/main.rs
@@ -3,7 +3,7 @@ use tortuga_guest::{Body, HostRequest, Response, Status};
 fn run(request: &mut HostRequest) -> Result<Response<impl Body>, std::io::Error> {
     let mut response = Response::default();
 
-    std::io::copy(request, &mut response)?;
+    std::io::copy(request.body(), response.body())?;
 
     response.set_status(Status::Created);
 
