8b76823296406ca5e079af0a921f8542b0287e5e
Miguel D. Salcedo <misalcedo@github.com>
2023-12-21 11:41:06 -0500

    Add graceful shutdown to the server and a time limit on child processes.

--- src/server.rs
+++ src/server.rs
@@ -2,33 +2,41 @@ use std::io;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::process::Stdio;
+use std::sync::Arc;
+use std::time::Duration;
 use tokio::io::{AsyncWriteExt, BufReader, BufWriter};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::process::Command;
 use tokio::runtime::Runtime;
 use tokio::{pin, select};
+use tokio_util::sync::CancellationToken;
 
 use crate::about;
 
-pub struct Server {
-    runtime: Runtime,
-    listener: TcpListener,
+#[repr(transparent)]
+#[derive(Clone)]
+pub struct ShutdownSignal(CancellationToken);
+
+impl ShutdownSignal {
+    pub fn shutdown(self) {
+        self.0.cancel()
+    }
 }
 
-impl Server {
-    pub fn new(address: SocketAddr) -> io::Result<Self> {
-        let runtime = tokio::runtime::Builder::new_current_thread()
-            .enable_io()
-            .build()?;
-        let listener = runtime.block_on(TcpListener::bind(address))?;
+struct ServerContext {
+    script: PathBuf,
+    script_path: PathBuf,
+    ip_address: String,
+    port: String,
+    path: &'static str,
+    software: String,
+    signature: String,
+}
 
-        Ok(Self { runtime, listener })
-    }
-
-    pub fn serve(self, script: PathBuf) -> io::Result<()> {
+impl ServerContext {
+    fn new(address: SocketAddr, script: PathBuf) -> io::Result<Self> {
         let script_path = script.canonicalize()?;
 
-        let address = self.listener.local_addr()?;
         let ip_address = address.ip().to_string();
         let port = address.port().to_string();
 
@@ -40,79 +48,140 @@ impl Server {
             software, ip_address, port
         );
 
-        self.runtime.block_on(async {
-            loop {
-                let (mut client, remote_address) = self.listener.accept().await?;
-                let mut command = Command::new(&script_path);
-
-                command
-                    .env_clear()
-                    .env("PATH", path)
-                    .env("SERVER_SOFTWARE", software.as_str())
-                    .env("SERVER_SIGNATURE", signature.as_str())
-                    .env("GATEWAY_INTERFACE", "CGI/1.1")
-                    .env("SERVER_PROTOCOL", "HTTP/1.1")
-                    .env("SCRIPT_FILENAME", script_path.as_os_str())
-                    .env("SCRIPT_NAME", script.as_os_str())
-                    .env("SERVER_ADDR", ip_address.as_str())
-                    .env("SERVER_PORT", port.as_str())
-                    .env("REMOTE_ADDR", remote_address.ip().to_string())
-                    .env("REMOTE_PORT", remote_address.port().to_string());
-
-                tokio::spawn(async move {
-                    {
-                        let (mut reader, mut writer) = client.split();
-
-                        let mut child = command
-                            .stdin(Stdio::piped())
-                            .stdout(Stdio::piped())
-                            .stderr(Stdio::inherit())
-                            .spawn()?;
-
-                        let mut stdin = child.stdin.take().map(BufWriter::new);
-                        let mut stdout = child.stdout.take().map(BufReader::new);
-
-                        let reader_task = async {
-                            if let Some(mut stdin) = stdin.as_mut() {
-                                tokio::io::copy(&mut reader, &mut stdin).await
-                            } else {
-                                Ok(0)
-                            }
-                        };
-
-                        let writer_task = async {
-                            if let Some(mut stdout) = stdout.as_mut() {
-                                tokio::io::copy(&mut stdout, &mut writer).await
-                            } else {
-                                Ok(0)
-                            }
-                        };
-
-                        pin!(reader_task);
-                        pin!(writer_task);
-
-                        let mut reader_done = false;
-                        let mut writer_done = false;
-
-                        loop {
-                            select! {
-                                _ = child.wait() => {
-                                    break;
-                                }
-                                _ = &mut reader_task, if !reader_done => {
-                                    reader_done = true;
-                                }
-                                _ = &mut writer_task, if !writer_done => {
-                                    writer_done = true;
-                                }
-                            }
-                        }
-                    }
-
-                    client.shutdown().await
-                });
-            }
+        Ok(Self {
+            script,
+            script_path,
+            ip_address,
+            port,
+            path,
+            software,
+            signature,
         })
+    }
+}
+
+pub struct Server {
+    runtime: Runtime,
+    listener: TcpListener,
+    shutdown: ShutdownSignal,
+}
+
+impl Server {
+    pub fn new(address: SocketAddr) -> io::Result<Self> {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_io()
+            .enable_time()
+            .build()?;
+        let listener = runtime.block_on(TcpListener::bind(address))?;
+        let shutdown = ShutdownSignal(CancellationToken::new());
+
+        Ok(Self {
+            runtime,
+            listener,
+            shutdown,
+        })
+    }
+
+    pub fn shutdown(&self) -> ShutdownSignal {
+        self.shutdown.clone()
+    }
+
+    pub fn serve(self, script: PathBuf) -> io::Result<()> {
+        let address = self.listener.local_addr()?;
+        let context = Arc::new(ServerContext::new(address, script)?);
+
+        let result = self.runtime.block_on(async {
+            loop {
+                let (client, remote_address) = select! {
+                    _ = self.shutdown.0.cancelled() => {
+                        break;
+                    }
+                    result = self.listener.accept() => {
+                        result?
+                    }
+                };
+
+                tokio::spawn(Self::handle_client(context.clone(), remote_address, client));
+            }
+
+            Ok(())
+        });
+
+        self.runtime.shutdown_timeout(Duration::from_secs(5));
+
+        result
+    }
+
+    async fn handle_client(
+        context: Arc<ServerContext>,
+        remote_address: SocketAddr,
+        mut client: TcpStream,
+    ) -> io::Result<()> {
+        {
+            let (mut reader, mut writer) = client.split();
+            let mut child = Command::new(&context.script_path)
+                .kill_on_drop(true)
+                .env_clear()
+                .env("PATH", &context.path)
+                .env("SERVER_SOFTWARE", &context.software.as_str())
+                .env("SERVER_SIGNATURE", &context.signature.as_str())
+                .env("GATEWAY_INTERFACE", "CGI/1.1")
+                .env("SERVER_PROTOCOL", "HTTP/1.1")
+                .env("SCRIPT_FILENAME", &context.script_path.as_os_str())
+                .env("SCRIPT_NAME", &context.script.as_os_str())
+                .env("SERVER_ADDR", &context.ip_address.as_str())
+                .env("SERVER_PORT", &context.port.as_str())
+                .env("REMOTE_ADDR", remote_address.ip().to_string())
+                .env("REMOTE_PORT", remote_address.port().to_string())
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::inherit())
+                .spawn()?;
+
+            let mut stdin = child.stdin.take().map(BufWriter::new);
+            let mut stdout = child.stdout.take().map(BufReader::new);
+
+            let reader_task = async {
+                if let Some(mut stdin) = stdin.as_mut() {
+                    tokio::io::copy(&mut reader, &mut stdin).await
+                } else {
+                    Ok(0)
+                }
+            };
+
+            let writer_task = async {
+                if let Some(mut stdout) = stdout.as_mut() {
+                    tokio::io::copy(&mut stdout, &mut writer).await
+                } else {
+                    Ok(0)
+                }
+            };
+
+            pin!(reader_task);
+            pin!(writer_task);
+
+            let mut reader_done = false;
+            let mut writer_done = false;
+
+            loop {
+                select! {
+                    _ = child.wait() => {
+                        break;
+                    }
+                    _ = &mut reader_task, if !reader_done => {
+                        reader_done = true;
+                    }
+                    _ = &mut writer_task, if !writer_done => {
+                        writer_done = true;
+                    }
+                    _ = tokio::time::sleep(Duration::from_secs(1)) => {
+                        child.start_kill()?;
+                    }
+                }
+            }
+        }
+
+        client.shutdown().await
     }
 }
 
@@ -127,6 +196,7 @@ mod tests {
     #[test]
     fn new_connections() {
         let server = Server::new(SocketAddr::from(([127, 0, 0, 1], 0))).unwrap();
+        let signal = server.shutdown();
         let address = server.listener.local_addr().unwrap();
 
         let thread = thread::spawn(|| server.serve("./examples/hello.cgi".into()));
@@ -136,6 +206,9 @@ mod tests {
 
         client.write_all(b"Hi!").unwrap();
         client.read_to_string(&mut output).unwrap();
+
+        signal.shutdown();
+        thread.join().unwrap().unwrap();
 
         assert_eq!(output.as_str(), "\nHello, World!\n");
     }
