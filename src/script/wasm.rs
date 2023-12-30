c29d008e6531112a3f6e907d5f76850bd61d00ca
Miguel D. Salcedo <misalcedo@github.com>
2023-12-30 13:02:59 -0500

    Initial attempt at a wasm script handler. However, still getting an error.

--- src/script/wasm.rs
+++ src/script/wasm.rs
@@ -1,17 +1,22 @@
 use crate::context::RequestContext;
 use bytes::Bytes;
 use std::io;
+use std::path::PathBuf;
 use wasi_common::pipe::{ReadPipe, WritePipe};
 use wasmtime::{Config, Engine, Linker, Module, Store};
 use wasmtime_wasi::WasiCtxBuilder;
 
 pub async fn serve(context: RequestContext, body: Bytes) -> io::Result<Bytes> {
-    serve_wasmtime(context, body)
+    serve_wasmtime(context.script()?, &context, body)
         .await
         .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
 }
 
-async fn serve_wasmtime(context: RequestContext, body: Bytes) -> Result<Bytes, wasmtime::Error> {
+async fn serve_wasmtime(
+    file: &PathBuf,
+    context: &RequestContext,
+    body: Bytes,
+) -> Result<Bytes, wasmtime::Error> {
     let mut configuration = Config::new();
 
     configuration
@@ -19,27 +24,39 @@ async fn serve_wasmtime(context: RequestContext, body: Bytes) -> Result<Bytes, w
         .consume_fuel(true)
         .parallel_compilation(true);
 
-    let engine = Engine::default();
+    let engine = Engine::new(&configuration)?;
     let mut linker = Linker::new(&engine);
     wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
 
     let stdout = WritePipe::new_in_memory();
-    let wasi = WasiCtxBuilder::new()
+    let mut builder = WasiCtxBuilder::new();
+
+    for argument in context.arguments() {
+        builder.arg(argument)?;
+    }
+
+    for (variable, value) in context.variables() {
+        builder.env(variable, value)?;
+    }
+
+    let wasi = builder
         .stdin(Box::new(ReadPipe::from(body.as_ref())))
         .stdout(Box::new(stdout.clone()))
         .inherit_stderr()
         .build();
+
     let mut store = Store::new(&engine, wasi);
 
     store.set_fuel(1_000_000)?;
     store.fuel_async_yield_interval(Some(1_000))?;
 
-    let module = Module::from_file(&engine, "target/wasm32-wasi/debug/wasi.wasm")?;
-    linker.module(&mut store, "", &module)?;
+    let module = Module::from_file(&engine, file)?;
+    linker.module_async(&mut store, "", &module).await?;
     linker
         .get_default(&mut store, "")?
         .typed::<(), ()>(&store)?
-        .call(&mut store, ())?;
+        .call_async(&mut store, ())
+        .await?;
 
     Ok(Bytes::new())
 }
--- src/server/handler.rs
+++ src/server/handler.rs
@@ -6,7 +6,6 @@ use http::StatusCode;
 use http_body_util::Full;
 use hyper::{Request, Response};
 use std::io;
-use std::mem::size_of;
 use std::sync::Arc;
 
 pub struct CgiHandler {
@@ -48,106 +47,5 @@ impl CgiHandler {
         }
 
         Ok(response)
-    }
-}
-
-fn decode_path(s: &str) -> Result<String, &str> {
-    if !s.contains('%') {
-        return Err(s);
-    }
-
-    let mut path = Vec::with_capacity(s.len());
-    let mut buffer = [0u8; size_of::<char>()];
-    let mut character = String::with_capacity(2);
-    let mut characters = s.chars();
-
-    while let Some(c) = characters.next() {
-        match c {
-            '+' => {
-                path.extend_from_slice(' '.encode_utf8(&mut buffer).as_bytes());
-            }
-            '%' => match (characters.next(), characters.next()) {
-                (Some(a), Some(b)) => {
-                    character.clear();
-                    character.push(a);
-                    character.push(b);
-
-                    match u8::from_str_radix(character.as_str(), 16) {
-                        Ok(decoded) => path.push(decoded),
-                        Err(_) => return Err(s),
-                    }
-                }
-                _ => return Err(s),
-            },
-            _ => {
-                path.extend_from_slice(c.encode_utf8(&mut buffer).as_bytes());
-            }
-        }
-    }
-
-    String::from_utf8(path).map_err(|_| s)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn special_characters() {
-        assert_eq!(decode_path("abc"), Err("abc"));
-        assert_eq!(decode_path("%2"), Err("%2"));
-        assert_eq!(decode_path("%20%26").unwrap(), " &");
-        assert_eq!(decode_path("%C6%92").unwrap(), "ƒ");
-    }
-
-    #[test]
-    fn empty() {
-        let mut headers = [httparse::EMPTY_HEADER];
-
-        let input = b"\r\n";
-        let result = httparse::parse_headers(input, &mut headers).unwrap();
-
-        assert!(result.is_complete());
-        assert_eq!(headers[0], httparse::EMPTY_HEADER);
-    }
-
-    #[test]
-    fn header_per_line() {
-        let mut headers = [httparse::EMPTY_HEADER];
-
-        let input = b"Content-Length: 42\n";
-        let result = httparse::parse_headers(input, &mut headers).unwrap();
-
-        assert!(result.is_partial());
-        assert_eq!(headers[0].name, "Content-Length");
-        assert_eq!(headers[0].value, b"42");
-    }
-
-    #[test]
-    fn header_per_line_complete() {
-        let mut headers = [httparse::EMPTY_HEADER];
-
-        let input = b"Content-Length: 42\r\n\r\n";
-        let result = httparse::parse_headers(input, &mut headers).unwrap();
-
-        assert!(result.is_complete());
-        assert_eq!(result.unwrap().0, input.len());
-        assert_eq!(headers[0].name, "Content-Length");
-        assert_eq!(headers[0].value, b"42");
-    }
-
-    #[test]
-    fn complete_with_body() {
-        let mut headers = [httparse::EMPTY_HEADER];
-
-        let input = b"Foo: Bar\r\n\r\nbody";
-        let result = httparse::parse_headers(input, &mut headers).unwrap();
-        let start_index = result.unwrap().0;
-
-        assert!(result.is_complete());
-        assert_eq!(start_index, input.strip_suffix(b"body").unwrap().len());
-        assert_eq!(&input[start_index..], b"body");
-        assert_eq!(headers[0].name, "Foo");
-        assert_eq!(headers[0].value, b"Bar");
     }
 }
