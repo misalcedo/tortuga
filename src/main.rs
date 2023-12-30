a0811e2885a6c272d577720769f28820ce893211
Miguel D. Salcedo <misalcedo@github.com>
2023-12-30 11:47:19 -0500

    Remove unnecessary invoke implementation as it does not handle wasm.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use clap::{Parser, Subcommand};
 use http::uri::Uri;
-use std::collections::HashMap;
 use std::path::Component::CurDir;
 use std::path::PathBuf;
 
 mod about;
-mod cgi;
 mod context;
 mod script;
 mod server;
@@ -90,58 +88,8 @@ pub fn main() {
                 })
                 .expect("Unable to start the server");
         }
-        Some(Commands::Invoke(options)) => {
-            let args = vec!["-test", "echo hello"];
-            let env = HashMap::from([
-                ("PATH", env!("PATH")),
-                (
-                    "SERVER_SIGNATURE",
-                    "<address>Apache/2.4.41 (Ubuntu) Server at localhost Port 80</address>\n",
-                ),
-                ("SERVER_SOFTWARE", "Apache/2.4.41 (Ubuntu)"),
-                ("SERVER_NAME", "localhost"),
-                ("SERVER_ADDR", "::1"),
-                ("SERVER_PORT", "80"),
-                ("REMOTE_ADDR", "::1"),
-                ("DOCUMENT_ROOT", "/var/www/html"),
-                ("CONTEXT_PREFIX", "/cgi-bin/"),
-                ("CONTEXT_DOCUMENT_ROOT", "/usr/lib/cgi-bin/"),
-                ("SERVER_ADMIN", "webmaster@localhost"),
-                ("SCRIPT_FILENAME", "/usr/lib/cgi-bin/debug.cgi"),
-                ("REMOTE_PORT", "55914"),
-                ("GATEWAY_INTERFACE", "CGI/1.1"),
-                ("SERVER_PROTOCOL", "HTTP/1.1"),
-                // HTTP
-                ("HTTP_HOST", "localhost"),
-                ("HTTP_USER_AGENT", "curl/7.68.0"),
-                ("HTTP_ACCEPT", "*/*"),
-                ("CONTENT_LENGTH", "11"),
-                ("CONTENT_TYPE", "application/x-www-form-urlencoded"),
-                ("REQUEST_SCHEME", "http"),
-                ("REQUEST_METHOD", "POST"),
-                ("SCRIPT_NAME", "/cgi-bin/debug.cgi"),
-                ("QUERY_STRING", "foo+bar+--me%202"),
-                (
-                    "REQUEST_URI",
-                    "/cgi-bin/debug.cgi/extra/path?foo+bar+--me%202",
-                ),
-                // Only if there is a path after the script portion of the path.
-                // Translates the extra path based on the rules of the server to a local path.
-                ("PATH_INFO", "/extra/path"),
-                ("PATH_TRANSLATED", "/var/www/html/extra/path"),
-            ]);
-
-            let child = cgi::spawn(&options.script, args, env).expect("Failed to read stdout");
-            let output = child
-                .wait_with_output()
-                .expect("Failed to wait for the child process.");
-
-            let stdout = String::from_utf8_lossy(&output.stdout);
-            let stderr = String::from_utf8_lossy(&output.stderr);
-
-            println!("Exit code: {}", output.status);
-            println!("STDOUT:\n{stdout}");
-            println!("STDERR:\n{stderr}");
+        Some(Commands::Invoke(_options)) => {
+            todo!()
         }
         _ => {}
     }
