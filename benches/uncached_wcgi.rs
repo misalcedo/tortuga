e01f9a59a7c787bdb80aef2401f9428fa8f6bd67
Miguel D. Salcedo <misalcedo@github.com>
2023-12-31 11:33:33 -0500

    Update the benchmark to not have an explicit main function.

--- benches/uncached_wcgi.rs
+++ benches/uncached_wcgi.rs
@@ -8,46 +8,43 @@ use tortuga::{Options, Server};
 
 static URI: OnceLock<String> = OnceLock::new();
 
-#[allow(dead_code)]
-fn main() {
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("Unable to start an async runtime");
+// #[allow(dead_code)]
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("uncached wcgi", |b| {
+        let runtime = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .expect("Unable to start an async runtime");
 
-    let options = Options {
-        wasm_cache: None,
-        document_root: PathBuf::from("examples/"),
-        cgi_bin: PathBuf::from(CurDir.as_os_str()),
-        hostname: "localhost".to_string(),
-        port: 0,
-    };
-    let server = runtime.block_on(Server::bind(options)).unwrap();
+        let options = Options {
+            wasm_cache: None,
+            document_root: PathBuf::from("examples/"),
+            cgi_bin: PathBuf::from(CurDir.as_os_str()),
+            hostname: "localhost".to_string(),
+            port: 0,
+        };
+        let server = runtime.block_on(Server::bind(options)).unwrap();
 
-    URI.get_or_init(|| {
         let address = server.address().unwrap();
-        format!("http://{}/cgi-bin/assert.cgi", address)
+
+        URI.set(format!("http://{}/cgi-bin/assert.cgi", address)).unwrap();
+
+        runtime.spawn(async move { server.serve().await });
+
+        let client = reqwest::blocking::Client::new();
+
+        b.iter(move || {
+            let body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            let response = client.post(URI.get().unwrap())
+                .body(body)
+                .send()
+                .unwrap();
+
+            assert_eq!(StatusCode::OK, response.status());
+            assert_eq!(Bytes::from(body.as_bytes()), response.bytes().unwrap());
+        })
     });
-
-    runtime.spawn(async move { server.serve().await });
-
-    pub fn criterion_benchmark(c: &mut Criterion) {
-        c.bench_function("uncached wcgi", |b| {
-            let client = reqwest::blocking::Client::new();
-
-            b.iter(move || {
-                let body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-                let response = client.post(URI.get().unwrap())
-                    .body(body)
-                    .send()
-                    .unwrap();
-
-                assert_eq!(StatusCode::OK, response.status());
-                assert_eq!(Bytes::from(body.as_bytes()), response.bytes().unwrap());
-            })
-        });
-    }
-
-    criterion_group!(benches, criterion_benchmark);
-    criterion_main!(benches);
 }
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
