461765f7e2852adce079ade1aaba3ccb2dbcf40f
Miguel D. Salcedo <misalcedo@github.com>
2023-12-31 11:41:08 -0500

    Add content type to the benchmark.

--- benches/uncached_wcgi.rs
+++ benches/uncached_wcgi.rs
@@ -37,6 +37,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             let body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
             let response = client.post(URI.get().unwrap())
                 .body(body)
+                .header(reqwest::header::CONTENT_TYPE, "text/html")
                 .send()
                 .unwrap();
 
