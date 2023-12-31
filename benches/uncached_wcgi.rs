d4fb080100d0713f91559fb2ce214a637edeb292
Miguel D. Salcedo <misalcedo@github.com>
2023-12-31 14:05:59 -0500

    Move starting the server and setting the address outside the bench function.

--- benches/uncached_wcgi.rs
+++ benches/uncached_wcgi.rs
@@ -9,24 +9,26 @@ use tortuga::{Options, Server};
 static URI: OnceLock<String> = OnceLock::new();
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("uncached wcgi", |b| {
-        let runtime = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .expect("Unable to start an async runtime");
-        let options = Options {
-            wasm_cache: None,
-            document_root: PathBuf::from("examples/"),
-            cgi_bin: PathBuf::from(CurDir.as_os_str()),
-            hostname: "localhost".to_string(),
-            port: 0,
-        };
-        let server = runtime.block_on(Server::bind(options)).unwrap();
-        let address = server.address().unwrap();
-        let client = reqwest::blocking::Client::new();
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Unable to start an async runtime");
+    let options = Options {
+        wasm_cache: None,
+        document_root: PathBuf::from("examples/"),
+        cgi_bin: PathBuf::from(CurDir.as_os_str()),
+        hostname: "localhost".to_string(),
+        port: 0,
+    };
+    let server = runtime.block_on(Server::bind(options)).unwrap();
+    let address = server.address().unwrap();
 
-        URI.set(format!("http://{}/cgi-bin/assert.cgi", address)).unwrap();
-        runtime.spawn(async move { server.serve().await });
+    URI.set(format!("http://{}/cgi-bin/assert.cgi", address)).unwrap();
+
+    runtime.spawn(async move { server.serve().await });
+
+    c.bench_function("uncached wcgi", |b| {
+        let client = reqwest::blocking::Client::new();
 
         b.iter(move || {
             let body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
